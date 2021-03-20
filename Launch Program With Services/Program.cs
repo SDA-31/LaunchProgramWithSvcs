fd9907855a143b75e77d5926a18b324d33b26e0f
SDA <sereden.kod.312003@gmail.com>
2021-03-20 22:28:54 +0300

    Update some things

--- Launch Program With Services/Program.cs
+++ Launch Program With Services/Program.cs
@@ -13,11 +13,13 @@ namespace Launch_Program_Scs
             try
             {
                 if (args.Length == 0)
-                    throw new ArgumentException();
+                    throw new Exception();
                 sc = (from i in ArgsProcessing.ScsFrom(args) select new ServiceController(i)).ToList();
                 sc = sc.Where(n => n.Status.ToString().Equals("Stopped")).ToList();
                 if (sc.Count != 0)
                     ArgsProcessing.IsServices = true;
+                else
+                    throw new Exception();
             }
             catch { sc = null; }
             if (ArgsProcessing.IsServices)
---
+++ Launch Program With Services/README.md
@@ -0,0 +1,11 @@
+# LaunchProgramWithSvcs
+
+The program, as the name suggests, launches the desired application after activating the services specified in the arguments.
+
+## How to use this ~sh*t~ program:
+1. Create shortcut
+2. Enter the properties of shortcut
+3. In "Object" textbox, input the arguments.
+
+## Arguments:
+"Path to file" "Path to program" "Services to start" "Arguments to program"
